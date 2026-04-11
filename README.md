b36ef7900beb9e9f25464a698e36bfb70bd2e793
Ziomiu <67629294+Ziomiu@users.noreply.github.com>
2026-04-11 16:29:23 +0200

    Added simple visualization
    - Added simple visualization using streamlit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # MiSS_2026
 Repository for project on MSiS ( Modelowanie i Symulacja Systemów - Modeling and Simulation of Systems ). The focus of the project is to simulate spread of diseases in the population
+
+To run the app use:
+`streamlit run app.py
+`
--- app.py
+++ app.py
@@ -1,22 +1,60 @@
+import time
+
 from model import EpidemicModel
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 
-population = 50
-grid_size = 20
-infection_prob = 0.2
-recovery_time = 10
-steps = 10
+st.title("Epidemic Simulation")
 
-model = EpidemicModel(
-    population,
-    grid_size,
-    grid_size,
-    infection_prob,
-    recovery_time
-)
+population = st.sidebar.slider("Population", 50, 500, 200)
+grid_size = st.sidebar.slider("Grid size", 10, 50, 25)
+infection_prob = st.sidebar.slider("Infection probability", 0.0, 1.0, 0.2)
+recovery_time = st.sidebar.slider("Recovery time", 1, 20, 5)
+steps = st.sidebar.slider("Steps", 10, 200, 100)
 
-for step in range(steps):
-    print(f"Step: {step}")
-    model.step()
-    data = model.datacollector.get_model_vars_dataframe()
-    print(data.iloc[-1])
-    print("---------------------------")
+if st.button("Start Simulation"):
+
+    model = EpidemicModel(
+        population,
+        grid_size,
+        grid_size,
+        infection_prob,
+        recovery_time
+    )
+
+    grid_placeholder = st.empty()
+
+    for _ in range(steps):
+        time.sleep(0.5)
+        model.step()
+
+        grid = np.zeros((grid_size, grid_size,3))
+
+        for agent in model.scheduler.agents:
+            x, y = agent.pos
+
+            if agent.state == "S":
+                grid[x][y] = [0, 0, 1]
+            elif agent.state == "I":
+                grid[x][y] = [1, 0, 0]
+            elif agent.state == "R":
+                grid[x][y] = [0, 1, 0]
+
+        fig, ax = plt.subplots()
+        ax.imshow(grid)
+
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+        legend_patches = [
+            mpatches.Patch(color='blue', label='Susceptible'),
+            mpatches.Patch(color='red', label='Infected'),
+            mpatches.Patch(color='green', label='Recovered')
+        ]
+
+        ax.legend(handles=legend_patches, loc='upper right')
+
+        grid_placeholder.pyplot(fig)
+        plt.close(fig)
